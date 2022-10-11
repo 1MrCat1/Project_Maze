9ad533bfd7416c2fb8517b40d8d7ef98a8a45774
1MrCat1 <1MrCat1@users.noreply.github.com>
2022-10-11 14:26:27 +0300

    Created and tested rotation script

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float rotationSpeedX = 0.5f;
+    public float rotationSpeedY = 0.5f;
+    public float rotationSpeedZ = 0.5f;
+    private float rotationKeyX;
+    private float rotationKeyY;
+    private float rotationKeyZ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationKeyX = Input.GetAxis("Vertical");
+        rotationKeyZ = Input.GetAxis("Horizontal");
+        rotationKeyY = Input.GetAxis("Horizontal_2");
+        if(Math.Abs(rotationKeyX)>0.001){
+            Debug.Log("X: " + rotationKeyX);
+        }
+        if(Math.Abs(rotationKeyX)>0.001){
+            Debug.Log("Y: " + rotationKeyX);
+        }
+        if(Math.Abs(rotationKeyZ)>0.001){
+            Debug.Log("Z: " + rotationKeyZ);
+        }
+        transform.Rotate(rotationSpeedX*rotationKeyX,rotationKeyY*rotationSpeedY,-rotationSpeedZ*rotationKeyZ,Space.Self);
+        if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("w key was pressed");
+        }
+    }
+}
